6cd8d6eb9c5901f33704e3c629d82b0815bfe69c
CML <chrismanuel.lorando@gmail.com>
2026-01-28 17:28:33 +0700

    feat: update CommentSeeder to create comments based on tasks and associated users

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -9,6 +9,20 @@ class CommentSeeder extends Seeder
 {
     public function run(): void
     {
-        Comment::factory(50)->create();
+        $tasks = \App\Models\Task::all();
+
+        foreach ($tasks as $task) {
+            $users = collect();
+            $users->push(\App\Models\User::where('role', 'admin')->first());
+            $users->push($task->developer);
+            $users = $users->filter()->unique('id');
+
+            foreach (range(1, rand(1, 5)) as $i) {
+                Comment::factory()->create([
+                    'task_id' => $task->id,
+                    'user_id' => $users->random()->id,
+                ]);
+            }
+        }
     }
 }
