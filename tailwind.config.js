a550f18bde3d729ddad209118f87ee9c54751884
CML <chrismanuel.lorando@gmail.com>
2026-01-28 14:19:04 +0700

    feat: add custom styles for markdown elements and update Tailwind config paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-import preset from './vendor/filament/support/tailwind.config.preset'
+import preset from "./vendor/filament/support/tailwind.config.preset";
 
 export default {
     presets: [preset],
     content: [
-        './app/Filament/**/*.php',
-        './resources/views/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
+        "./app/Filament/**/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/views/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+        "./vendor/danharrin/filament-blog/resources/views/**/*.blade.php",
     ],
-}
+    plugins: [require("@tailwindcss/typography")],
+};
