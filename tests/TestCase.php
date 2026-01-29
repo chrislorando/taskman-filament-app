07afd19b58cb7142f6d121370f30d02839e941be
CML <chrismanuel.lorando@gmail.com>
2026-01-30 04:12:27 +0700

    test: implement safety check for optimized config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUpTraits()
+    {
+        if (app()->configurationIsCached()) {
+            $this->fail(
+                "CRITICAL: Application is in optimized mode (Config Cached).\n" .
+                "Testing halted to prevent data corruption.\n" .
+                "Run 'php artisan optimize:clear' first."
+            );
+        }
+
+        return parent::setUpTraits();
+    }
 }
